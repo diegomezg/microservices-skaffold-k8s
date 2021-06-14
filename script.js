ca6ee452b09df2e79efad95da9ddff98924c9b2f
Daniel Flores <daniel.flores@digitalonus.com>
2021-06-14 18:17:35 -0500

    Adding virtual users in test script

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export let options = {
+  vus: 20,
+  duration: '40s',
+};
+
 export default function () {
   http.get('http://20.83.18.0');
   sleep(1);
